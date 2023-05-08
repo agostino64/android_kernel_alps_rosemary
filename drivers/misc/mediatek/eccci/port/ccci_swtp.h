c2626f5408403f49e5c9bdd1bff7c4d4ec92792a
Matsvei Niaverau <matvej2002@gmail.com>
2023-05-08 13:52:39 +0200

    drivers: mediatek: eccci: Import Xiaomi changes
    * From rosemary-r-oss

--- drivers/misc/mediatek/eccci/port/ccci_swtp.h
+++ drivers/misc/mediatek/eccci/port/ccci_swtp.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2015 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 
@@ -12,7 +13,7 @@
  */
 #define MAX_PIN_NUM 5
 #define SWTP_COMPATIBLE_DEVICE_ID "mediatek, swtp-eint"
-#define SWTP1_COMPATIBLE_DEVICE_ID "mediatek, swtp1-eint"
+#define SWTP1_COMPATIBLE_DEVICE_ID "mediatek, swtp_1-eint"
 #define SWTP2_COMPATIBLE_DEVICE_ID "mediatek, swtp2-eint"
 #define SWTP3_COMPATIBLE_DEVICE_ID "mediatek, swtp3-eint"
 #define SWTP4_COMPATIBLE_DEVICE_ID "mediatek, swtp4-eint"
@@ -25,7 +26,6 @@
 
 struct swtp_t {
 	unsigned int	md_id;
-	unsigned int	curr_mode;
 	unsigned int	irq[MAX_PIN_NUM];
 	unsigned int	gpiopin[MAX_PIN_NUM];
 	unsigned int	setdebounce[MAX_PIN_NUM];
@@ -34,11 +34,9 @@ struct swtp_t {
 	int	tx_power_mode;
 	spinlock_t		spinlock;
 	struct delayed_work delayed_work;
-	struct delayed_work init_delayed_work;
 };
 /*****************************************************************************/
 /* External API Region called by ccci_swtp object */
 /*****************************************************************************/
 extern int swtp_init(int md_id);
-extern void inject_pin_status_event(int pin_value, const char pin_name[]);
 #endif				/* __SWTP_H__ */
