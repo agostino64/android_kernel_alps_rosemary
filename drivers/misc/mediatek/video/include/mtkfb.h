e574360b77f83c363ececb1fd886f8e7abbb9355
aarch64 <gutierrezaverruz0@hotmail.com>
2024-01-29 17:01:32 -0600

    drivers/misc/mediatek/video: Import OEM changes
    Signed-off-by: Agustin Gutierrez <gutierrezaverruz0@hotmail.com>

--- drivers/misc/mediatek/video/include/mtkfb.h
+++ drivers/misc/mediatek/video/include/mtkfb.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Copyright (c) 2019 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Author: Joey Pan <joey.pan@mediatek.com>
  */
 
@@ -382,6 +383,9 @@ struct mtkfb_device {
 	/* Linux fbdev framework data */
 	struct fb_info *fb_info;
 	struct device *dev;
+  
+        atomic_t resume_pending;
+	wait_queue_head_t resume_wait_q;
 
 	/* Android native fence support */
 	struct workqueue_struct *update_ovls_wq;
@@ -390,6 +394,8 @@ struct mtkfb_device {
 	int timeline_max;
 	struct list_head pending_configs;
 	struct ion_client *ion_client;
+	
+	bool is_prim_panel;
 };
 
 #endif				/* __KERNEL__ */
@@ -414,5 +420,7 @@ extern unsigned int vramsize;
 extern char *saved_command_line;
 #endif
 #endif
+
+int mtkfb_prim_panel_unblank(int timeout);
 
 #endif
