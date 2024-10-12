f0e2ef234836497935cea16b29f86aa3ccb9052c
Agustin Gutierrrez <gutierrezaverruz0@hotmail.com>
2024-10-11 19:41:24 -0600

    Revert "{drivers,sound}: mediatek: Support legacy audio firmware/blobs"
    This reverts commit 0fda10a384b90c0267bdbc43c07f74f6c0450e71.

--- sound/soc/mediatek/audio_dsp/v2/audio_buf.h
+++ sound/soc/mediatek/audio_dsp/v2/audio_buf.h
@@ -69,20 +69,14 @@ struct audio_buffer {
 	int period_count; /* preoid size with this hw buffer */
 };
 
-#if IS_ENABLED(CONFIG_MTK_AUDIODSP_LEGACY)
-#define audio_hw_buffer_type int
-#else
-#define audio_hw_buffer_type char
-#endif
-
 struct audio_hw_buffer {
 	struct audio_buffer aud_buffer;
-	audio_hw_buffer_type hw_buffer;
-	audio_hw_buffer_type audio_memiftype;   /*DL 1,2,3 */
-	audio_hw_buffer_type irq_num; /* irq with this hw buffer */
-	audio_hw_buffer_type memory_type; /* sram,dram */
+	char hw_buffer;
+	char audio_memiftype;   /*DL 1,2,3 */
+	char irq_num; /* irq with this hw buffer */
+	char memory_type; /* sram,dram */
 	int counter;
-	audio_hw_buffer_type ignore_irq;
+	char ignore_irq;
 };
 
 struct audiohw_buffer_ops {
